63d0e1c4d999f309d078d7a16cff52de76b6e94c
Jimmy J. Pecho Malqui <jpmjim@gmail.com>
2022-05-30 20:08:01 -0500

    conectar nuestra aplicacion a una API

--- src/components/Profile.svelte
+++ src/components/Profile.svelte
@@ -1,3 +1,8 @@
+<script>
+  export let nickname;
+  export let name;
+</script>
+
 <style>
   .Profile-content {
     display: flex;
@@ -29,8 +34,8 @@
       <img src="https://scontent.fjau2-1.fna.fbcdn.net/v/t1.6435-9/121559242_102078698353190_1030495291590614354_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=174925&_nc_ohc=jdSI20FZ5DUAX-W35IJ&_nc_ht=scontent.fjau2-1.fna&oh=00_AT93b22dXyAptZghz8dOX0lwX8S4MxT5LdKwNM5wGZSeoQ&oe=62BC5B6D" alt="">
     </div>
     <div class="Profile-info">
-      <h2>jpmjim</h2>
-      <span>Jimmy Pecho</span>
+      <h2>{nickname}</h2>
+      <span>{name}</span>
     </div>
   </div>
 </div>
--- src/containers/App.svelte
+++ src/containers/App.svelte
@@ -1,8 +1,16 @@
 <script>
+  import { onMount } from 'svelte';
   import Header from '../components/Header.svelte';
   import Main from '../components/Main.svelte';
   import TimeLine from '../components/TimeLine.svelte';
   import Sidebar from '../components/Sidebar.svelte';
+
+  let data = {};
+  const API = "https://kittygram-api.vercel.app/"
+  onMount(async () => {
+    const response = await fetch('API');
+    data = await response.json();
+  });
 </script>
 
 <style>
@@ -23,6 +31,6 @@
 
 <Header />
 <Main >
-  <TimeLine />
-  <Sidebar />
+  <TimeLine posts={data.posts}/>
+  <Sidebar {...data.user}/>
 </Main>
